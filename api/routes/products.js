f06f129e26f6e03fc6857627af3b4ccfff04b3d8
Muhammad Rehan <rehan@NANO-PC6>
2020-07-05 11:04:20 +0400

    Product Add - Get | in mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,8 +1,13 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/products');
+
 
 router.get('/', (req, res, next) => {
+
     res.status(200).json({
         message: 'Handling Products Requests'
     })
@@ -10,24 +15,44 @@ router.get('/', (req, res, next) => {
 
 
 router.post('/', (req, res, next) => {
-    const product = {
-      name: req.body.name,
-      price: req.body.price,
-    }
-    console.log(req.body)
 
-    res.status(201).json({
-        message: 'Handling Products Requests POST',
-        createdProduct:product
-    })
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+    product.save()
+        .then((result) => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Product Created',
+                createdProduct: product
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        message: "Responsind to Id",
-        id: id
-    })
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log(doc);
+            if(doc){
+                res.status(200).json(doc);
+            }else{
+                res.status(401).json({message: 'No valid entry found for given ID'});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
 });
 
 router.patch('/:productId', (req, res, next) => {
--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-const express = require('express'); 
+const express = require('express');
 const app = express();
 const morgen = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const productRoutes = require('./api/routes/products');
-const orderRoutes = require ('./api/routes/orders');
+const orderRoutes = require('./api/routes/orders');
 
 app.use(morgen('dev'));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//Mongo
+
+mongoose.connect('mongodb+srv://admin:' + process.env.MONGO_ATLAS_PW + '@node-ecom-cluster.jmrtf.mongodb.net/' + process.env.MONGO_DB_NAME + '?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
 //CORS 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authrization');
-    if (req.method == 'OPTIONS'){
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authrization');
+    if (req.method == 'OPTIONS') {
         res.header('Access-Control-Allow-Header', 'PUT, GET, POST, PATCH, DELETE');
         return res.status(200).json({});
     }
@@ -25,17 +35,17 @@ app.use((req, res, next)=>{
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     const error = new Error('Not Found');
     error.status = 404;
     next(error);
 })
 
-app.use((error, req, res, next)=>{
+app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
-        error:{
-            message: error.message,                       
+        error: {
+            message: error.message,
         }
     });
 });
