e0dc7aff95bd23aa4e7aaa1c612def1299e0e2dc
Muhammad Rehan <rehan@NANO-PC6>
2020-07-06 17:50:59 +0400

    Order Creation

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,23 +1,39 @@
 const express = require('express');
 const route = express.Router();
+const mongoose = require('mongoose');
 
+const Order = require('../models/order');
 
 route.get('/', (req, res, next) => {
-  
+
     res.status(200).json({
-        message: "responce to order get"       
+        message: "responce to order get"
     })
 });
 
 route.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    }
-    res.status(201).json({
-        message: "responce to order post",
-        orderCreated: order
-    })
+
+    order.find()
+        .exec()
+        .then((result)=>{
+            res.status(200).json(result);
+        }).error();
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+    });
+    order.save()
+        .then((result) => {
+            console.log(result);
+            res.status(201).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 route.get('/:orderId', (req, res, next) => {
